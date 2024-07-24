46d80407f2753a4d98b03e34e45e2d7079e09f1d
Ranaeltoonsy <ranahany970@gmail.com>
2024-07-24 15:06:16 +0300

    edit3

---
+++ index.js
@@ -0,0 +1,16 @@
+document.write('<h2>Heading</h2>');
+document.write('<hr>')
+for(var num = 1; num <=6; num++){
+    document.write('<h'+num+'>This is header number'+num+'</h'+num+'>');
+}
+
+var sum = 0
+do{
+    var e= prompt("Enter a number:");
+    var n = +e;
+    if(!isNaN (n)){
+        sum += n;
+       
+    }
+}while( n !==0 && sum < 100);
+alert("the final total is" + sum);
